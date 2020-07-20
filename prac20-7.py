9e2adb80f44a9b7a3b8597537b609867d564e7be
Pritam Dhara <pritamdhara914@gmail.com>
2020-07-20 13:38:03 +0530

    prac1

---
+++ prac20-7.py
@@ -0,0 +1,18 @@
+# today I practiced 1.[Making a sum calculator using def] and 2.[using loop fn]
+
+#def
+'''
+def sum(a, b):
+    c = a+b
+    print("Here is the result: ")
+    return c
+
+f = sum(89, 11)
+print(f)
+'''
+
+# loop
+'''
+for u in range(21000, 66000):
+    print(u)
+'''    
