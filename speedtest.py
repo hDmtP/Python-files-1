5b8819d99bd0dfff05cacaa0dd6a4ef726b22071
Pritam Dhara <pritamdhara914@gmail.com>
2020-07-21 15:58:33 +0530

    version6

---
+++ speedtest.py
@@ -0,0 +1,6 @@
+#type-->  speedtest-cli --version, this will show you the version of speedtest*cli, python and bit
+#type-->  speedtest-cli ,this will show your internet service provider org(ISP), location(HOST), download speed, upload speed, altitude, IP add(probably)
+#type-->  speedtest-cli -h, to get all the available options
+#type-->  speedtest-cli --help
+#type-->  speedtest-cli --simple
+#type-->  speedtest-cli --share
--- tut5.py
+++ tut5.py
@@ -17,5 +17,28 @@ with sr.Microphone as source:
         print("You said:{}".format(text))  
     except:
         print("Sorry, I cannot recognize you:(")
-'''        
+'''       
+# print((2+3)*5/2%6)  #precedence in python
+
+# s = "Rooose"
+# v = {i for i in s}
+# print(len(v)) # ans = 4, cuz it counts different characters in the word
+
+# t = [0]
+# s1 = s2 = t
+# s1 = s1 + [1]
+# s2 += [1]
+# print(t)
+#Ans [0, 1]
+
+# s = [100, 84, 63, 97]
+# s_sort = s.sort()
+# print(s[::-1][0])  #Ans  It's 100, because when you sort it, this is s = [63, 84, 97, 100] then you reverse it with [start from 0:until the end of it: and substract 1 then the list would be [100, 97, 84, 63] and your are asking the value in index 0 so the value 100 is the right answer.
+
+# s = "apple"
+# x = s.find('p')
+# y = s.find('m')
+# z = s.find('')
+# print(x+y+z)  #0 since value of X is 1...value of Y is -1.....value of z is 0..
+
 
