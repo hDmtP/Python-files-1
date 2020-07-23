4ec8042af7b47dbfc06292b6b9554e4c2870760f
Pritam Dhara <pritamdhara914@gmail.com>
2020-07-23 16:07:41 +0530

    prac2

---
+++ prac23-7.py
@@ -0,0 +1,80 @@
+'''
+print("Kya kar rahe ho?", end="")
+print("Mai ab bohot kuch karne vala hu", end=" ")
+
+print("Knock knock! the devil is wihsing to come in.", end="")
+print("Call me venom....em", end=" ")
+
+print("lokrpggojo655fgogrkgrkgkgrkrgrorrkklkk", end=" ")
+print("khkfdm", end=" ")
+'''
+
+# print("QWhat is up","cool dude")
+'''
+var1 = "other fps games"
+var2 = "Apollo432 "
+print(var2+var1)
+'''
+
+# print(10*"Rap God\n")
+
+# var1 = 65965
+# var2 = 979
+# print(100*str(var1+var2))
+'''
+import math
+
+print("Enter your percentile\n")
+jeeRank = input()
+print("Your AIR:",math.ceil((1-float(jeeRank)/100)*900000))
+'''
+
+# myStr = "Dhara is a good boy"
+# print(myStr[: :-1])
+# print(myStr[: :-2])
+# print(myStr[: :-5])
+# print(myStr.isalnum())
+# print(myStr.isalpha())
+# print(myStr.endswith("Boy"))
+# print(myStr.count("o"))
+# print(myStr.find("oo"))
+# print(myStr.lower())
+# print(myStr.upper())
+# print(myStr.replace('o', 'q'))
+
+numbers = [1, 5, 7, 9, 45, 35, 87, 101, 15]
+# numbers.sort()
+# numbers.reverse()
+# print(numbers[ : :7])
+# print(numbers[ : :-1])
+# print(len(numbers))
+# numbers.append(78)
+# numbers.insert(0, 69)
+# numbers.pop()   #eliminates the last number
+# print(numbers)
+'''
+[]  list --> mutable
+() tuple --> immutable
+'''
+
+'''
+tp = (655) --> this is not a tuple
+tp = (655,) --> this is  a tuple
+'''
+
+# a = 56
+# b = 89
+
+# to interchange 'a' with 'b'
+'''
+1st process
+temp = a
+a = b
+b = temp
+print(a,b)
+'''
+'''
+# 2nd process
+a, b = b, a
+print(a,b)
+'''
