85b2f3d0eb29de3fa6e4490d8dbc4f72b24c34b1
Pritam Dhara <pritamdhara914@gmail.com>
2020-08-30 22:21:36 +0530

    version 1.2.2

---
+++ zipfile.py
@@ -0,0 +1,6 @@
+from pyzip import PyZip
+pyzip=PyZip()
+pyzip['key1']=b"content_bytes"
+pyzip['key2']=b"file_bytes"
+pyzip.save(r"C:\Users\user\Documents\OneDrive\python practice folder\file.zip")
+zip_bytes=pyzip.to_bytes()
