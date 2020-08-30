5da093de1103845ace88f732f6940e1bd0dcace9
Pritam Dhara <pritamdhara914@gmail.com>
2020-08-30 14:22:29 +0530

    version 1.4.0

--- MATPLOTLIB/bar-graph.py
+++ MATPLOTLIB/bar-graph.py
@@ -1,10 +1,10 @@
 from matplotlib import pyplot as plt
-n=int(input("How many players do you want?\n"))
+num=int(input("How many players do you want?\n"))
 no_of_Players=0
 players=[]
 scores=[]
 
-while no_of_Players!=n:
+while no_of_Players!=num:
     Player=str(input("Enter Player name: "))
     score=int(input("Enter Player's score: "))
     players.append(Player)
@@ -17,10 +17,11 @@ print(scores)
 print("Elements saved!\n")
 
 print("Do you want to see the bar graph?\n")
-confirm=str(input("(y/n): "))
+confirm=int(input("(yes=1/no=0): "))
+conFirm = bool(confirm)
 
 
-if confirm=='y':
+if conFirm==True:
     color=str(input("Which color will you pick for your bar graph?(red/blue/green/yellow/black): "))
     plt.bar(players, scores, color=color)
     plt.title('Score Card')
---
+++ MATPLOTLIB/plotly/2.py
@@ -0,0 +1,7 @@
+import plotly.express as px
+
+df = px.data.gapminder()
+
+fig = px.bar(df, x="continent", y="pop", color="continent",
+  animation_frame="year", animation_group="country", range_y=[0,4000000000])
+fig.show()
