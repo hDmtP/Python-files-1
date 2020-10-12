8cb5b2ff869cee642fe591ffd98caa2fd663d914
Pritam Dhara <pritamdhara914@gmail.com>
2020-10-12 08:57:50 +0530

    version 2.2.2

---
+++ table-chart-in.py
@@ -0,0 +1,7 @@
+from prettytable import PrettyTable
+
+myTable = PrettyTable(["CandidateName","GitHUb","GitHub contributions per yr","LinkedIN conections","Btech CGPA"])
+
+myTable.add_row(["Pritam Dhara","hDmtP",127,"20+",0.00])
+
+print(myTable)
