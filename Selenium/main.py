92eb9162735a23fa6f239fd05623a670aefc3573
Pritam Dhara <pritamdhara914@gmail.com>
2020-11-13 11:56:20 +0530

    v3.2.7

--- Selenium/main.py
+++ Selenium/main.py
@@ -3,4 +3,11 @@ from selenium.webdriver.common.keys import Keys
 
 chromedriver_path = r"C:\Users\user\Downloads\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chromedriver_path)
-driver.get(url="https://www.xnxx.com")
+driver.get(url="https://selenium-python.readthedocs.io/getting-started.html")
+elem = driver.find_element_by_name("q")
+print(elem)
+elem.clear()
+elem.send_keys("send_keys")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+# driver.close()
